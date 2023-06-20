72ebff0ca7ade8beaf73287d2e064aafdfff5212
muram-105 <muram8700@gmail.com>
2023-06-20 10:00:20 +0300

    add change

--- sync_script.py
+++ sync_script.py
@@ -1,22 +1,22 @@
-import subprocess
-import time
+# import subprocess
+# import time
 
-# Set the paths to the repositories
-repo1_path = '/home/maram/Github/Common'
-repo2_path = '/home/maram/Github/Test/Common'
+# # Set the paths to the repositories
+# repo1_path = '/home/maram/Github/Common'
+# repo2_path = '/home/maram/Github/Test/Common'
 
-# Set the delay in seconds between sync operations
-delay = 60  # 5 minutes
+# # Set the delay in seconds between sync operations
+# delay = 60  # 5 minutes
 
-def sync_repos():
-    # Change directory to repo1 and pull changes
-    subprocess.run(['git', '-C', repo1_path, 'pull'])
+# def sync_repos():
+#     # Change directory to repo1 and pull changes
+#     subprocess.run(['git', '-C', repo1_path, 'pull'])
 
-    # Change directory to repo2, add changes, commit, and push
-    subprocess.run(['git', '-C', repo2_path, 'add', '.'])
-    subprocess.run(['git', '-C', repo2_path, 'commit', '-m', 'Sync changes'])
-    subprocess.run(['git', '-C', repo2_path, 'push'])
+#     # Change directory to repo2, add changes, commit, and push
+#     subprocess.run(['git', '-C', repo2_path, 'add', '.'])
+#     subprocess.run(['git', '-C', repo2_path, 'commit', '-m', 'Sync changes'])
+#     subprocess.run(['git', '-C', repo2_path, 'push'])
 
-while True:
-    sync_repos()
-    time.sleep(delay)
+# while True:
+#     sync_repos()
+#     time.sleep(delay)
