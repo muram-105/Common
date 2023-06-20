027ee9bfa2db2fce2308d4f633baf3c5fe4ba3a8
muram-105 <muram8700@gmail.com>
2023-06-20 10:37:28 +0300

    add

--- pos_custom/models/pos_rder.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from odoo import _, api, fields, models, tools
-
-
-class PosOrder(models.Model):
-    _inherit = "pos.order"
-    
-    config_id = fields.Many2one(
-        'pos.config', string='Point of Sale',related='session_id.config_id',store=True) 
