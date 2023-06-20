d6151385c94ae7d29a2765db71ab32c3e2486907
muram-105 <muram8700@gmail.com>
2023-06-20 10:32:51 +0300

    add new module

---
+++ product_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Products Custom Fields",
+
+    'summary': """
+        Product Card Customization""",
+
+    'description': """Add fields to the product card""",
+    'category': 'Products',
+    'Author': 'Seventy Eight Systems',
+    'version': '16.0.0.1',
+    'depends': ['stock','product','sale'],
+    'data': [
+        'views/product_views.xml',
+    ],
+     'license': 'LGPL-3',
+}
